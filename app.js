c4e5a135d3bc375f3c7610821bbc59482d4be38b
t3ng221 <androsiam001@gmail.com>
2021-10-06 21:12:05 +0600

    second one

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const searchMeal = () => {
 
     const mealName = document.getElementById('input').value;
     console.log(mealName);
-    const url = `www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`
+    const url = `https://themealdb.com/api/json/v1/1/search.php?s=${mealName}`
     fetch(url)
     .then(res => res.json())
     .then(data => displayMeal(data));
-    console.log(data);
+}
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText=text;
 }
 
 const displayMeal=meal=>{
-    console.log(meal);
+    setInnerText('meal-details-sec',meal.mealName);
 }
-
